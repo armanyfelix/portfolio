54041d18b69f08f8b2b93ee57fe8ba0d87d0bb8c
lafv <armanyfelix@proton.me>
2025-12-18 01:10:07 -0800

    zero typescript errors but css completed broken

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from "@astrojs/tailwind";
+// @ts-check
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from "@tailwindcss/vite"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import node from "@astrojs/node"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
-});
+  output: "server",
+  site: "https://armanyfelix.dev",
+  integrations: [mdx(), sitemap(), react()],
+  vite: {
+    plugins: [
+      tailwind(),
+    ],
+  },
+  adapter: node({
+    mode: "standalone",
+  }),
+})
--- src/components/FloatingNav.tsx
+++ src/components/FloatingNav.tsx
@@ -23,7 +23,7 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     link: string;
-    icon?: JSX.Element | undefined;
+    icon?: any | undefined;
   }[];
   className?: string;
 }) => {
@@ -91,7 +91,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-neutral/[0.2] rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-4 pl-8 py-2 items-center justify-center space-x-4",
+          "flex max-w-fit fixed top-5 mx-auto inset-x-0 justify-self-center place-self-center border border-neutral/20 rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-5000 pr-4 pl-8 py-2 items-center justify-center space-x-4",
           className,
           background,
         )}
